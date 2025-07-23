35379a7368e307506b87e5bae73c47ca50409ba3
Siddhant-Shrestha <shrestha.sid01@gmail.com>
2025-07-23 10:32:22 +0545

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main(){
+	cout << "_________________________________________________________________________" << endl;
+	cout << "|\tGrocery\t\t|\tElectronics\t|\tClothes\t\t|" << endl;
+	cout << "|_______________________|_______________________|_______________________|" << endl;
+	
+}
+
+
+
